b93a852d11c57ee95333fcce92e5ec75b23f2b9d
Unyielding1 <david.verde.alvarez@gmail.com>
2022-05-31 19:15:06 -0500

    html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
  function component() {
    const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
    return element;
  }
