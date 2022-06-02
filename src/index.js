fe6da0ddaad88084323d5f64888691078f2d039c
Unyielding1 <david.verde.alvarez@gmail.com>
2022-06-02 15:26:36 -0500

    todo

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-<<<<<<< Updated upstream
-import _ from 'lodash';
-import printMe from './print.js';
-=======
 // eslint-disable-next-line no-unused-vars
 import _, {} from 'lodash';
 import * as method from './modules/interactive.js';
 import UpdateUi from './modules/mainSec.js';
 import './style.css';
 import { refresh } from './modules/selectors.js';
->>>>>>> Stashed changes
 
 method.updateSave();
 
-<<<<<<< Updated upstream
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-   return element;
- }
-
- document.body.appendChild(component());
-=======
 UpdateUi.enterBtn.addEventListener('click', (e) => {
   e.preventDefault();
   if (UpdateUi.input.value === '') {
@@ -35,4 +17,3 @@ UpdateUi.enterBtn.addEventListener('click', (e) => {
 });
 
 refresh.addEventListener('click', method.refresh);
->>>>>>> Stashed changes
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
-   entry: {
+  mode: 'development',
+  entry: {
      index: './src/index.js',
      print: './src/print.js',
    },
+   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
    output: {
      filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist'),
+     publicPath: '/',
    },
+   optimization: {
+    runtimeChunk: 'single',
+  },
  };
+ 
